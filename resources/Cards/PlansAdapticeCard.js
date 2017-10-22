e2b139d39bd949b03f04124b06e03c724f6b3c13
AkshayDhanuka <kdhanuka26@gmail.com>
2017-10-22 15:20:36 +0530

    changed card type

--- resources/Cards/PlansAdapticeCard.js
+++ resources/Cards/PlansAdapticeCard.js
@@ -1,43 +1,42 @@
 exports.allActions = {
-  contentType: "application/vnd.microsoft.card.adaptive",
-  content: {
-    speak: "Plan your future with us, by choosing, one of the following plans",
-    type: "AdaptiveCard",
-    version: "1.0",
-    body: [
-      {
-        type: "TextBlock",
-        text: "Plans",
-        size: "large",
-        weight: "bolder"
+  "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "type": "AdaptiveCard",
+    "version": "1.0",
+    "body": [{
+      "type": "container",
+      "items":[{
+            "type": "TextBlock",
+            "text": "Plans",
+            "size": "large",
+            "weight": "bolder"
       }
-    ],
-    actions: [
+      ]
+    }],
+    "actions": [
       {
-        type: "Action.Submit",
-        title: "Wedding Plans",
-        data: "Wedding Plans"
+        "type": "Action.Submit",
+        "title": "Wedding Plans",
+        "data": "Wedding Plans"
       },
       {
-        type: "Action.Submit",
-        title: "Retirement Plans",
-        data: "Retirement Plans"
+        "type": "Action.Submit",
+        "title": "Retirement Plans",
+        "data": "Retirement Plans"
       },
       {
-        type: "Action.Submit",
-        title: "Education Plans",
-        data: "Education Plans"
+        "type": "Action.Submit",
+        "title": "Education Plans",
+        "data": "Education Plans"
       },
       {
-        type: "Action.Submit",
-        title: "Personal Plans",
-        data: "PersonalPlans"
+        "type": "Action.Submit",
+        "title": "Personal Plans",
+        "data": "Personal Plans"
       },
       {
-        type: "Action.OpenUrl",
-        title: "Get your personalized plan",
-        url: "http://google.com"
+        "type": "Action.OpenUrl",
+        "title": "Get your personalized plan",
+        "url": "http://google.com"
       }
     ]
-  }
-};
+}
