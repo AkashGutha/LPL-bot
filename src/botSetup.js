f2415b74b6f3057ef656d1b5cdbee856df7da1c9
AkshayDhanuka <kdhanuka26@gmail.com>
2017-10-23 01:14:43 +0530

    added mime and fix

--- src/botSetup.js
+++ src/botSetup.js
@@ -6,6 +6,8 @@
 //Node Imports
 var restify = require("restify");
 var builder = require("botbuilder");
+var mime = require('mime');
+var fs = require("fs");
 
 //to load .env file
 require("dotenv").load();
@@ -15,20 +17,30 @@ var regeX = require("./regexIntents");
 
 // Setup Restify Server
 var server = restify.createServer();
+
 server.listen(process.env.port || process.env.PORT || 3978, function() {
   console.log("%s listening to %s", server.name, server.url);
 });
 
 // add the pubic directory to serve as the direct testing application
-server.get('/', restify.plugins.serveStatic({
-  directory: './public',
-  default: 'index.html'
-}))
+server.get(
+  "/",
+  restify.plugins.serveStatic({
+    directory: "./public",
+    default: "index.html"
+  })
+);
 
-server.get('/pdf', restify.plugins.serveStatic({
-  directory: './public',
-  default: 'plans.pdf'
-}))
+server.get("/pdf", function(req, res, next) {
+  fs.readFile(__dirname + "/../public/plans.pdf", function(err, data) {
+    if (err) res.send(200, err);
+    else {
+      res.contentType = mime.getType("pdf");
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+});
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
